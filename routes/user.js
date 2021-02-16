d852802bfaf4286145968ba27c00397c1e555d9a
Deen-Joseph <josephdeenp.f@gmail.com>
2021-02-16 11:37:14 +0530

    recent

--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const productHelpers = require('../helpers/product-helpers');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  let products=[
-    {
-    name:"Manchester City",
-    category:"Clubs",
-    description:"2015-16 Away Jersey",
-    image:"http://s3.amazonaws.com/nikeinc/assets/44662/Fa15_FB_WE_Club_Kits_PR_Match_Front_A_Manchester_City_R_native_1600.jpg?1437375840"
-  },
-  {
-    name:"Manchester United",
-    category:"Clubs",
-    description:"2020-21 Home Jesrey",
-    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.I5LgxiiERY_XDZgZs7e2YQHaEU%26pid%3DApi&f=1"
-  },
-  {
-    name:"Chelsea",
-    category:"Clubs",
-    description:"2020-21 Home Jersey",
-    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.4NmowWqkb9-Mr5WsP35m9AHaFS%26pid%3DApi&f=1"
-  },
-  {
-    name:"Everton",
-    category:"Clubs",
-    description:"2017-18 Third Jersey",
-    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.WqifaOnbETPEgZx_AUwLEQHaHa%26pid%3DApi&f=1"
-  }
-    
-  ]
-  res.render('index', { products,admin:false });
+  productHelpers.getAllProducts().then((products)=>{
+    console.log(products);
+    res.render('user/view-products',{products})
+  
+  })     
+  
 });
+router.get('/login',(req,res )=>{
+  res.render('user/login')
+
+})
+router.get('/signup',(req,res )=>{
+  res.render('user/signup')
+})
+router.post('/signup',(req,res)=>{
+   
+})
 
 module.exports = router;
